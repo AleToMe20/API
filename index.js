892e457d51cb20c4a05570607f3e1776e29bbc7d
AleToMe20 <78073236+AleToMe20@users.noreply.github.com>
2023-05-19 15:03:11 +0200

    Implemented MongoDB connection and integrated it into the existing codebase
    This commit introduces the integration of MongoDB into the existing code. The necessary changes have been made to establish a connection with MongoDB using mongoose. The code now includes CRUD operations for users and movies, allowing the creation, retrieval, updating, and deletion of user and movie data. The MongoDB connection enables the application to store and retrieve data from a persistent database, improving data management capabilities.

--- index.js
+++ index.js
@@ -3,196 +3,202 @@ const morgan = require("morgan");
 const fs = require("fs");
 const path = require("path");
 const app = express();
-uuid = require("uuid");
+const uuid = require("uuid");
+const mongoose = require('mongoose');
+const Models = require('./models.js');
+
+const Movies = Models.Movie;
+const Users = Models.User;
+
+mongoose.connect('mongodb://localhost:27017/mydatabase', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const accessLogStream = fs.createWriteStream(path.join(__dirname, "log.txt"), {
   flags: "a",
 });
 app.use(morgan("combined", { stream: accessLogStream }));
 
-let users = [
-  {
-    id: 1,
-    name: "Kim",
-    favoriteMovies: [],
-  },
-  {
-    id: 2,
-    name: "Adjei",
-    favoriteMovies: ["AIR"],
-  },
-];
-
-let movies = [
-  {
-    Title: "Lord of the rings",
-    Description:
-      "Set in the fictional world of Middle-earth, the films follow the hobbit Frodo Baggins as he and the Fellowship embark on a quest to destroy the One Ring, to ensure the destruction of its maker, the Dark Lord Sauron. The Fellowship eventually splits up and Frodo continues the quest with his loyal companion Sam and the treacherous Gollum. Meanwhile, Aragorn, heir in exile to the throne of Gondor, along with the elf Legolas, the dwarf Gimli, Merry, Pippin, and the wizard Gandalf, unite to save the Free Peoples of Middle-earth from the forces of Sauron and rally them in the War of the Ring to aid Frodo by distracting Sauron's attention.",
-    Genre: {
-      Name: "Fantasy",
-      Description:
-        "The Lord of the Rings is a series of three epic fantasy adventure films directed by Peter Jackson, based on the novel The Lord of the Rings by J. R. R. Tolkien.",
-    },
-    Director: {
-      Name: "Peter Jackson",
-      Bio: "Sir Peter Robert Jackson ONZ KNZM is a New Zealand film director, screenwriter and producer. He is best known as the director, writer and producer of the Lord of the Rings trilogy and the Hobbit trilogy, both of which are adapted from the novels of the same name by J. R. R. Tolkien.",
-      Birth: 1961,
-    },
-    ImgaeURL:
-      "https://irs.www.warnerbros.com/keyart-jpeg/movies/…of_the_rings_fellowship_of_the_ring_2000x3000.jpg",
-    Featured: "False",
-  },
-
-  {
-    Title: "AIR",
-    Description:
-      "From award-winning director Ben Affleck, AIR reveals the unbelievable game-changing partnership between a then-rookie Michael Jordan and Nike's fledgling basketball division which revolutionized the world of sports and contemporary culture with the Air Jordan brand. This moving story follows the career-defining gamble of an unconventional team with everything on the line, the uncompromising vision of a mother who knows the worth of her son’s immense talent, and the basketball phenom who would become the greatest of all time.",
-    Genre: {
-      Name: " Drama, Sports",
-      Description:
-        "A fact-based drama that no one will dunk on, Air aims to dramatize events that changed the sports world forever -- and hits almost nothing but net. Read critic reviews",
-    },
-    Director: {
-      Name: "Ben Affleck",
-      Bio: "Benjamin Géza Affleck is an American actor and filmmaker. His accolades include two Academy Awards. Affleck began his career as a child when he starred in the PBS educational series The Voyage of the Mimi. He later appeared in the independent comedy Dazed and Confused and various Kevin Smith films.",
-      Birth: 1972,
-    },
-    ImgaeURL:
-      "https://upload.wikimedia.org/wikipedia/en/d/de/AirFilmPoster.png",
-    Featured: "False",
-  },
-  {
-    Title: "65",
-    Description:
-      "After a catastrophic crash on an unknown planet, pilot Mills quickly discovers he's actually stranded on Earth -- 65 million years ago. Now, with only one chance at a rescue, Mills and the only other survivor, Koa, must make their way across an unknown terrain riddled with dangerous prehistoric creatures.",
-    Genre: {
-      Name: "Sci-fi, Adventure, Action, Mystery & thriller",
-      Description:
-        "Sodden sci-fi that somehow finds a way to bungle Adam Driver fighting dinosaurs, 65 is closer to zero",
-    },
-    Director: {
-      Name: "Scott Beck, Bryan Woods",
-      Bio: "Scott Beck is the author-illustrator of many highly praised picture books for young children. His books have received starred reviews and such praise as “a soaring story-hour selection” (School Library Journal) and “perfect for sharing one-on-one” (Kirkus Reviews).",
-      Birth: 1984,
-    },
-    ImgaeURL:
-      "https://upload.wikimedia.org/wikipedia/en/c/c4/65_film_teaser_poster.jpg",
-    Featured: "False",
-  },
-];
-
-//CREATE
-app.post("/users", (req, res) => {
-  const newUser = req.body;
-
-  if (newUser.name) {
-    newUser.id = uuid.v4();
-    users.push(newUser);
-    res.status(201).json(newUser);
-  } else {
-    res.status(400).send("user need names");
-  }
+// CREATE - Add a user
+app.post('/users', (req, res) => {
+  Users.findOne({ Username: req.body.Username })
+    .then((user) => {
+      if (user) {
+        return res.status(400).send(req.body.Username + 'already exists');
+      } else {
+        Users
+          .create({
+            Username: req.body.Username,
+            Password: req.body.Password,
+            Email: req.body.Email,
+            Birthday: req.body.Birthday
+          })
+          .then((user) =>{res.status(201).json(user) })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).send('Error: ' + error);
+        })
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
 });
 
-//UPDATE
-app.put("/users/:id", (req, res) => {
-  const { id } = req.params;
-  const updatedUser = req.body;
-
-  let user = users.find((user) => user.id == id);
-
-  if (user) {
-    user.name = updatedUser.name;
-    res.status(200).json(user);
-  } else {
-    res.status(400).send("no such user");
-  }
+// Get all users
+app.get('/users', (req, res) => {
+  Users.find()
+    .then((Users) => {
+      res.status(200).json(Users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
-// CREATE
-app.patch("/users/:id/favorites/:movieTitle", (req, res) => {
-  const { id, favoriteMovieTitle } = req.params;
-
-  let user = users.find((user) => user.id == id);
-
-  if (user) {
-    user.favoriteMovies.push(favoriteMovieTitle);
-    res.status(201).send("movie added to your favorites list");
-    console.log(favoriteMovieTitle);
-  } else {
-    res.status(400).send("movie not added");
-  }
+// Get a user by username
+app.get('/users/:username', (req, res) => {
+  Users.findOne({ username: req.params.username })
+    .then((user) => {
+      res.status(200).json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
-// DELETE
-app.delete("/users/:id/favorites/:movieTitle", (req, res) => {
+//updates a account holders information
+app.put('/users/:username', (req, res) => {
+  Users.findOneAndUpdate(
+    { username: req.params.username },
+    {
+      $set: {
+        Username: req.body.username,
+        Password: req.body.Password,
+        Email: req.body.Email,
+        Birthday: req.body.Birthday
+      }
+    },
+    { new: true }
+  )
+    .then((updatedUser) => {
+      if (!updatedUser) {
+        return res.status(404).send("Error: User doesn't exist");
+      } else {
+        res.json(updatedUser);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+// Add a movie to a user's list of favorites
+app.post('/users/:Username/movies/:MovieID', (req, res) => {
+  Users.findOneAndUpdate({ Username: req.params.Username }, {
+     $push: { FavoriteMovies: req.params.MovieID }
+   },
+   { new: true }, // This line makes sure that the updated document is returned
+  (err, updatedUser) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    } else {
+      res.json(updatedUser);
+    }
+  });
+});
+
+// DELETE - Remove a movie from a user's list of favorites
+app.delete("/users/:id/favorites/:movieID", (req, res) => {
   const { id, movieTitle } = req.params;
-  let user = users.find((user) => user.id == id);
-  if (user) {
-    user.favoriteMovies = user.favoriteMovies.filter(
-      (title) => title !== movieTitle
-    );
-    res
-      .status(200)
-      .send(`${movieTitle} has been removed to user ${id}'s array`);
-  } else {
-    res.status(400).send("no such user");
-  }
+  Users.findByIdAndUpdate(id, {
+    $pull: { FavoriteMovies: movieTitle }
+  }, { new: true })
+    .then((updatedUser) => {
+      res.status(200).send(`${movieTitle} has been removed from user ${id}'s favorites.`);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
-// DELETE
-app.delete("/users/:id", (req, res) => {
-  const { id } = req.params;
-  let user = users.find((user) => user.id == id);
-  if (user) {
-    users = users.filter((user) => user.id != id);
-    res.status(200).send(`user ${id} has been deleted`);
-  } else {
-    res.status(400).send("no such user");
-  }
+// DELETE - Delete a user by username
+app.delete('/users/:username', (req, res) => {
+  Users.findOneAndRemove({ username: req.params.username })
+    .then((user) => {
+      if (!user) {
+        res.status(400).send(req.params.username + ' was not found');
+      } else {
+        res.status(200).send(req.params.username + ' was deleted.');
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
-//Read
+// READ - Get all movies
 app.get("/movies", (req, res) => {
-  res.status(200).json(movies);
+  Movies.find()
+    .then((movies) => {
+      res.status(200).json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
-//Read
+// READ - Get a movie by title
 app.get("/movies/:title", (req, res) => {
-  const { title } = req.params;
-  const movie = movies.find((movie) => movie.Title === title);
-
-  if (movie) {
-    res.status(200).json(movie);
-  } else {
-    res.status(400).send("no such movie");
-  }
+  Movies.findOne({ title: req.params.title })
+    .then((movie) => {
+      if (!movie) {
+        res.status(400).send("No such movie");
+      } else {
+        res.status(200).json(movie);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
-//Read
-app.get("/movies/genre/:genreName", (req, res) => {
-  const { genreName } = req.params;
-  const genre = movies.find((movie) => movie.Genre.Name === genreName).Genre;
-
-  if (genre) {
-    res.status(200).json(genre);
-  } else {
-    res.status(400).send("no such genre");
-  }
+// READ - Get a genre by name
+app.get("/movies/genre/:genrename", (req, res) => {
+  Movies.find({ "genre.name": { $regex: new RegExp(req.params.genrename, "i") } })
+    .then((genre) => {
+      if (!genre || genre.length === 0) {
+        res.status(400).send("No such genre");
+      } else {
+        res.status(200).json(genre);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
-//Read
-app.get("/movies/director/:directorName", (req, res) => {
-  const { directorName } = req.params;
-  const director = movies.find(
-    (movie) => movie.Director.Name === directorName
-  ).Director;
-
-  if (director) {
-    res.status(200).json(director);
-  } else {
-    res.status(400).send("no such director");
-  }
+// READ - Get a director by name
+app.get("/movies/director/:directorname", (req, res) => {
+  Movies.find({ "director.name": req.params.directorname })
+    .then((director) => {
+      if (!director || director.length === 0) {
+        res.status(400).send("No such director");
+      } else {
+        res.status(200).json(director);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
-app.listen(8080, () => console.log("Server  listening on port 8080"));
+app.listen(8080, () => console.log("Server listening on port 8080"));
