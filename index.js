c403220b260802266ee2284849730a82e1e6b4c5
AleToMe20 <78073236+AleToMe20@users.noreply.github.com>
2023-08-30 13:22:46 +0200

    added CORS

--- index.js
+++ index.js
@@ -3,19 +3,35 @@ const morgan = require("morgan");
 const fs = require("fs");
 const path = require("path");
 const bodyParser = require('body-parser');
-const app = express();
+const cors = require('cors');
 const uuid = require("uuid");
 const mongoose = require('mongoose');
 const Models = require('./models.js');
 const { check, validationResult } = require('express-validator');
 
+const app = express();
 const Movies = Models.Movie;
 const Users = Models.User;
 const Genres = Models.Genre;
 const Directors = Models.Director;
+
 const databaseUrl = "mongodb://localhost:27017/myFlixDB" 
 
 mongoose.connect(process.env.CONNECTION_URI || databaseUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// let allowedOrigins = "*";
+// app.use(cors({
+//   origin: (origin, callback) => {
+
+//     if(!origin) return callback(null, true);
+//     if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+//       let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+//       return callback(new Error(message ), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
+app.use(cors());
 
 // Parsing incoming requests
 app.use(bodyParser.json());
